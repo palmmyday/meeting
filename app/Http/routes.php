72e4308b9ec795c7a808b709b0894ec73deeeead
palmmyday <phatthakorn.palm@mail.sit.kmutt.ac.th>
2017-04-14 21:08:25 +0700

    Fix path

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,9 @@
 |
 */
 
-Route::get('/', 'MyController@index');
+Route::get('/', 'MyControllers@index');
 
-Route::get('home', 'MyController@index');
+Route::get('home', 'Mycontrollers@index');
 
 Route::get('done', 'MyControllers@done');
 
